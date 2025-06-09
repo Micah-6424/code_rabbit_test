75fb2c30c61354663ec302e4830b8022d910ae2a
Micah Gagerman <micahgagerman21@gmail.com>
2025-06-09 15:52:43 -0500

    e

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,7 +23,7 @@ export default function Login() {
       });
 
       const data = await res.json();
-
+ 
       if (!res.ok) {
         throw new Error(data.error || 'Something went wrong');
       }
